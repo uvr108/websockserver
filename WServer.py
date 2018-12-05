5c5d4b001d54f90ba512aa899cd36d96c1702677
ulises <dpineda@csn.uchile.cl>
2018-12-05 14:31:24 -0500

    Se agregaron funcionalidades

--- WServer.py
+++ WServer.py
@@ -1,16 +1,27 @@
 import asyncio
 import websockets
+import sys
+import json
+from funciones import comandos
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+async def opt(websocket, path):
+    #option = await websocket.recv()
+    option=json.loads(await websocket.recv())
+    opt = [*option.keys()][0] 
+    args=[] 
+    if opt in ['sumar','multiplicar']:
+        args = [*option.values()][0] 
+    elif opt == 'palabras':
+        args.append(option[opt][0])
 
-    greeting = f"Hello {name}!"
+    print(f"< {opt} {args}")
+    output = comandos.get(opt)(*args)
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+    msgstr = json.dumps({'respuesta':output}) 
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+    await websocket.send(msgstr)
+
+start_server = websockets.serve(opt, 'localhost', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
