141cce73b9dc5e7d91dbbb78c17cc597f0ddddde
ulises <dpineda@csn.uchile.cl>
2018-12-05 14:30:54 -0500

    Se agregaron funcionalidades

--- WSclient.py
+++ WSclient.py
@@ -1,16 +1,30 @@
 import asyncio
 import websockets
+from funciones import comandos
+import json
 
-async def hello():
+async def opt():
     async with websockets.connect(
             'ws://localhost:8765') as websocket:
-        name = input("What's your name? ")
+        print(f'{[*comandos.keys()]}')
 
-        await websocket.send(name)
-        print(f"> {name}")
+        option = input("Ingrese su opcion ? ")
+        args=[]
+
+        if option == 'sumar':
+            lis = input('ingrese lista de numeros : ')
+            args=json.loads(lis)
+        elif option == 'multiplicar':
+            lis = input('ingrese lista de numeros : ')
+            args=json.loads(lis)
+        elif option == 'palabras':
+            frase = input('ingrese su frase : ')
+            args.append(frase)
+        msg = json.dumps({option:args})
+        await websocket.send(msg)
+        print(f"> {option}")
 
         greeting = await websocket.recv()
         print(f"< {greeting}")
 
-asyncio.get_event_loop().run_until_complete(hello())
-
+asyncio.get_event_loop().run_until_complete(opt())
