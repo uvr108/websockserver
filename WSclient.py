3b689d22cd8768d0da2cea0bb145b24ddf470826
ulises <dpineda@csn.uchile.cl>
2018-12-06 12:37:40 -0500

    Se agrego coneccion a rethinkdb

--- WSclient.py
+++ WSclient.py
@@ -2,6 +2,9 @@ import asyncio
 import websockets
 from funciones import comandos
 import json
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError
+from rthkdb.continuos import Continuos
 
 async def opt():
     async with websockets.connect(
@@ -20,6 +23,19 @@ async def opt():
         elif option == 'palabras':
             frase = input('ingrese su frase : ')
             args.append(frase)
+        elif option == 'listar':
+            tabla = input('ingrese tabla : ')
+            kw={'table':tabla}
+            flag = ''
+            where = {}
+            indice = {'anomes':[2018,10]}
+            order = 'sfile'
+            distinct = None
+            pluck = ['action','latitud','longitud','operador','m1_magnitud','no']
+            get_all = {'flag':flag,'where': where, 'indice':indice, 'order':order,'pluck':pluck,'distinct': distinct }
+            kw.update({'get_all':get_all})
+            args.append(kw)
+
         msg = json.dumps({option:args})
         await websocket.send(msg)
         print(f"> {option}")
