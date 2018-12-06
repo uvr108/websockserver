2a508dbc872ea902b3bfed58c4b22ed1f99bcd85
ulises <dpineda@csn.uchile.cl>
2018-12-06 12:38:14 -0500

    Se agrego coneccion a rethinkdb

--- funciones.py
+++ funciones.py
@@ -1,4 +1,11 @@
 from functools import reduce
+from rthkdb.continuos import Continuos
+import rethinkdb as r
+import os
+
+rt_host=os.environ['rt_host']
+rt_port=os.environ['rt_port']
+rt_db=os.environ['rt_db']
 
 def sumar(*lista):
     return sum(lista) 
@@ -11,8 +18,16 @@ def palabras(frase):
     pl = frase.split()
     return len(pl)
 
+def listar(kw):
+   
+    cons=Continuos()
+    conn = r.connect(host=rt_host, port=rt_port, db=rt_db)
+    data = cons.ejecutar(r,**kw)
+    conn.close()
+    cons.__del__()
+    return data
 
-comandos = { 'sumar': sumar, 'multiplicar': multiplicar, 'palabras': palabras }
+comandos = { 'sumar': sumar, 'multiplicar': multiplicar, 'palabras': palabras, 'listar': listar }
 
 
 
