ab871e8b408a1342429c86475205ff95e53f599b
ulises <dpineda@csn.uchile.cl>
2018-12-20 08:07:26 -0500

    Se grego loop de conexion

--- WSclient.py
+++ WSclient.py
@@ -9,38 +9,45 @@ from rthkdb.continuos import Continuos
 async def opt():
     async with websockets.connect(
             'ws://10.54.218.19:8765') as websocket:
-        print(f'{[*comandos.keys()]}')
 
-        option = input("Ingrese su opcion ? ")
-        args=[]
 
-        if option == 'sumar':
-            lis = input('ingrese lista de numeros : ')
-            args=json.loads(lis)
-        elif option == 'multiplicar':
-            lis = input('ingrese lista de numeros : ')
-            args=json.loads(lis)
-        elif option == 'palabras':
-            frase = input('ingrese su frase : ')
-            args.append(frase)
-        elif option == 'listar':
-            tabla = input('ingrese tabla : ')
-            kw={'table':tabla}
-            flag = ''
-            where = {}
-            indice = {'anomes':[2018,10]}
-            order = 'sfile'
-            distinct = None
-            pluck = ['action','latitud','longitud','operador','m1_magnitud','no']
-            get_all = {'flag':flag,'where': where, 'indice':indice, 'order':order,'pluck':pluck,'distinct': distinct }
-            kw.update({'get_all':get_all})
-            args.append(kw)
+        while (1):
 
-        msg = json.dumps({option:args})
-        await websocket.send(msg)
-        print(f"> {option}")
+            print(f'{[*comandos.keys()]}')
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+            option = input("Ingrese su opcion ? ")
+ 
+            args=[]
+
+            if option == 'sumar':
+                lis = input('ingrese lista de numeros : ')
+                args=json.loads(lis)
+            elif option == 'multiplicar':
+                lis = input('ingrese lista de numeros : ')
+                args=json.loads(lis)
+            elif option == 'palabras':
+                frase = input('ingrese su frase : ')
+                args.append(frase)
+            elif option == 'listar':
+                tabla = input('ingrese tabla : ')
+                kw={'table':tabla}
+                flag = ''
+                where = {'action':'NEW'}
+                indice = {'anomes':[2018,10]}
+                order = 'sfile'
+                distinct = None
+                pluck = ['action','latitud','longitud','operador','m1_magnitud','no']
+                get_all = {'flag':flag,'where': where, 'indice':indice, 'order':order,'pluck':pluck,'distinct': distinct }
+                kw.update({'get_all':get_all})
+                args.append(kw)
+            elif option == 'salir':
+               break
+
+            msg = json.dumps({"command":option, "message":args})
+            await websocket.send(msg)
+            print(f"> {option}")
+
+            greeting = await websocket.recv()
+            print(f"< {greeting}")
 
 asyncio.get_event_loop().run_until_complete(opt())
