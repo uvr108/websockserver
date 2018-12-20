5b74760c69e9120b767562fcb657851bd7c88903
ulises <dpineda@csn.uchile.cl>
2018-12-20 08:08:52 -0500

    Se grego loop de conexion

--- pruebas.py
+++ pruebas.py
@@ -10,39 +10,44 @@ def main():
 
     print(f'{[*comandos.keys()]}')
 
-    option = input('Ingrese opcion : ')
-    print(f'Su opcion es {option}')
 
     args = []
 
-    if option == 'sumar':
-        lis = input('ingrese lista de numeros : ')
-        args=json.loads(lis)
-    elif option == 'multiplicar':
-        lis = input('ingrese lista de numeros : ')
-        args=json.loads(lis) 
-    elif option == 'palabras':
-        frase = input('ingrese su frase : ')
-        args.append(frase)
-    elif option == 'listar':
-        tabla = input('ingrese tabla : ')
-        kw={'table':tabla}
-        flag = ''
-        where = {}
-        indice = {'anomes':[2018,10]}
-        order = 'sfile'
-        distinct = None
-        pluck = ['action','latitud','longitud','operador','m1_magnitud','no']
-        get_all = {'flag':flag,'where': where, 'indice':indice, 'order':order,'pluck':pluck,'distinct': distinct }
-        kw.update({'get_all':get_all})
-        args.append(kw)
+    while (1):
 
-    return comandos.get(option)(*args)
+        option = input('Ingrese opcion : ')
+        print(f'Su opcion es {option}')
+
+        if option == 'salir':
+            break
+        elif option == 'sumar':
+            lis = input('ingrese lista de numeros : ')
+            args=json.loads(lis)
+        elif option == 'multiplicar':
+            lis = input('ingrese lista de numeros : ')
+            args=json.loads(lis) 
+        elif option == 'palabras':
+            frase = input('ingrese su frase : ')
+            args.append(frase)
+        elif option == 'listar':
+            tabla = input('ingrese tabla : ')
+            kw={'table':tabla}
+            flag = ''
+            where = {}
+            indice = {'anomes':[2018,10]}
+            order = 'sfile'
+            distinct = None
+            pluck = ['action','latitud','longitud','operador','m1_magnitud','no']
+            get_all = {'flag':flag,'where': where, 'indice':indice, 'order':order,'pluck':pluck,'distinct': distinct }
+            kw.update({'get_all':get_all})
+            args.append(kw)
+
+        res = comandos.get(option)(*args)
+        print(f"{res}")
 
 if __name__ == "__main__":
 
-    res = main()
-    print(f"{res}")
+    main()
 
 
      
