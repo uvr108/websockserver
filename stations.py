80a3ed0cbe43e3bd41d89e30c47de5a39c7e3031
uvergara <uvergara@csn.uchile.cl>
2019-02-19 16:06:29 +0000

    Commit Inicial

---
+++ stations.py
@@ -0,0 +1,34 @@
+from orm_collector.manager import SessionCollector
+from Extraer.extraer import ejecuta
+
+from networktools.colorprint import bprint, gprint, rprint
+
+import pyproj
+
+
+def obtener():
+    out = [] 
+    s=SessionCollector()
+    station = s.get_stations() 
+
+    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+    
+    for s in station:
+                
+        lon, lat, alt = pyproj.transform(ecef, lla, float(s.position_x),float(s.position_y),float(s.position_z), radians=False)
+               
+        stat = {"command":"enviar","code":s.code,"name":s.name, "x":lon, "y":lat, "z":alt, "check": False}
+        msg = {"stream":'testws','payload':stat}
+        out.append(msg)
+
+    return out 
+
+
+
+if __name__ == "__main__":
+    
+    for o in obtener():
+        print(o)
+
+
