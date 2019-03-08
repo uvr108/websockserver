1ed2fb25e48c3ff5317d86469dac9e5736f819e6
uvergara <uvergara@csn.uchile.cl>
2019-03-08 20:58:14 +0000

    Se agregaron varios

--- WServer.py
+++ WServer.py
@@ -1,4 +1,5 @@
 import asyncio
+# import nest_asyncio
 import websockets
 import datetime
 import json
@@ -13,25 +14,39 @@ async def opt(websocket, path):
         vector = []
         print("Empezando a recibir data")
         option = json.loads(await websocket.recv())
-        
+       
+        print('RECIVO : ',option)
+ 
         opt = option.get('command')
-        
+        tipo = option.get('tipo')
         args = option.get('message')
 
-        output = comandos.get(opt)(*args)
+        print("option tipo args : ", option, tipo, args)
 
-        print("##########################")
 
-        if option.get('command') == 'listar':
-
+        if tipo == 'sql':
+            output = await comandos.get(opt)(*args)
             msgstr = json.dumps(genlista(output))
-        else:
-            msgstr = json.dumps(output)
+
+        elif tipo == 'file':
+           
+            print('ARGS : ',args)
+            
+            # nest_asyncio.apply()
+           
+            station = ['VALN','PVCA']
+            filedir = os.environ['RDB_FILEDIR']
+            di = "2019-01-07T00:00:00+00:00"
+            df = "2019-01-07T01:59:59+00:00"
+ 
+            output = await comandos.get(opt)(station,filedir,di,df)
+            msgstr =  json.dumps(output) 
         
         await websocket.send(msgstr)
-        
-        
-start_server = websockets.serve(opt, '10.54.218.167', '8765')
+
+ 
+start_server = websockets.serve(opt, '10.54.218.195', '8765')
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
